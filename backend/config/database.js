3ea67df2026fd106d71bb4454768c3b720b719e7
Abdul Kareem <abdulkareem98979897@gmail.com>
2026-07-13 01:38:37 +0530

    updated databasejs

--- backend/config/database.js
+++ backend/config/database.js
@@ -2,16 +2,12 @@ const mysql = require("mysql2");
 require("dotenv").config();
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  uri: process.env.DATABASE_URL,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
   ssl: {
     ca: process.env.DB_CA_CERT,
-    rejectUnauthorized: true,
   },
 });
 
